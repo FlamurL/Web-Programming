1623389f0e267519274747ed1316dd5d8a646174
flimani <flamurlimani004@gmail.com>
2025-10-26 17:25:44 +0100

    first-lab

---
+++ Labs/lab/src/main/java/mk/ukim/finki/wp/lab/model/Book.java
@@ -0,0 +1,40 @@
+package mk.ukim.finki.wp.lab.model;
+
+import lombok.Data;
+
+@Data
+public class Book {
+    String title;
+    String genre;
+    double averageRating;
+
+    public Book(String title, String genre, double averageRating) {
+        this.title = title;
+        this.genre = genre;
+        this.averageRating = averageRating;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public double getAverageRating() {
+        return averageRating;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public void setAverageRating(double averageRating) {
+        this.averageRating = averageRating;
+    }
+}
---
+++ Labs/lab1/lab/lab/src/main/java/mk/ukim/finki/wp/lab/repository/InMemoryBookRepository.java
@@ -0,0 +1,21 @@
+package mk.ukim.finki.wp.lab.repository;
+
+import mk.ukim.finki.wp.lab.bootstrap.DataHolder;
+import mk.ukim.finki.wp.lab.model.Book;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class InMemoryBookRepository implements BookRepository {
+
+    @Override
+    public List<Book> findAll() {
+        return DataHolder.books;
+    }
+
+    @Override
+    public List<Book> searchBooks(String text, Double rating) {
+        return DataHolder.books.stream()
+                .filter(book -> book.getTitle().contains(text) && book.getAverageRating() >= rating)
+                .collect(Collectors.toList());
+    }
+}
