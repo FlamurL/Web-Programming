103db946c7b8d0c17adece9472446eedac787563
flimani <flamurlimani004@gmail.com>
2025-10-30 19:27:42 +0100

    comm

--- Labs/lab1/lab/lab/src/main/java/mk/ukim/finki/wp/lab/bootstrap/DataHolder.java
+++
@@ -1,8 +0,0 @@
-package mk.ukim.finki.wp.lab.bootstrap;
-import java.util.*;
-import mk.ukim.finki.wp.lab.model.Book;
-import mk.ukim.finki.wp.lab.model.BookReservation;
-public class DataHolder {
-    public static List<Book> books = new ArrayList<>(10);
-    public static List<BookReservation> reservations = new ArrayList<>();
-}
