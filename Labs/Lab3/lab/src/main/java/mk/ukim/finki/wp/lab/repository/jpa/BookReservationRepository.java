e090da3fa4fd559e17e9ff14bec1dfc695364f88
Flamur Limani <75123450+FlamurL@users.noreply.github.com>
2025-12-03 12:10:15 +0100

    Delete Labs/Lab3 directory

--- Labs/Lab3/lab/src/main/java/mk/ukim/finki/wp/lab/repository/jpa/BookReservationRepository.java
+++
@@ -1,7 +0,0 @@
-package mk.ukim.finki.wp.lab.repository.jpa;
-
-import mk.ukim.finki.wp.lab.model.BookReservation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookReservationRepository extends JpaRepository<BookReservation, Long> {
-}
